538ba6176f6d0c5c272211b3438037a9c94a4753
Ricky Collins <richardcollins52317@gmail.com>
2018-08-15 14:53:00 -0400

    trying something new

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
+require 'mysql2'
+require 'aws-sdk'
 
+
+client = Mysql2::Client.new(:username => ENV['RDS_USERNAME'], :password => ENV['RDS_PASSWORD'], :host => ENV['RDS_HOSTNAME'], :port => ENV['RDS_PORT'], :database => ENV['RDS_DB_NAME'], :sock => '/tmp/mysql.sock')
+
+load 'local_ENV.rb' if File.exist?('local_ENV.rb')
 enable :sessions
 
 get '/' do
-  erb :index, :layout => :layout 
+  'Hello World!'
+  # erb :index, :layout => :layout 
+end
+
+post '/add' do
+
 end
