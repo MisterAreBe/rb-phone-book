bfe38c892d6b4360bfd5535ed08adee46263987a
Ricky Collins <richardcollins52317@gmail.com>
2018-08-17 18:40:54 -0400

    added ui to take input, and store contacts

--- app.rb
+++ app.rb
@@ -2,17 +2,40 @@ require 'sinatra'
 require 'mysql2'
 require 'aws-sdk'
 
-
-client = Mysql2::Client.new(:username => ENV['RDS_USERNAME'], :password => ENV['RDS_PASSWORD'], :host => ENV['RDS_HOSTNAME'], :port => ENV['RDS_PORT'], :database => ENV['RDS_DB_NAME'], :sock => '/tmp/mysql.sock')
-
 load 'local_ENV.rb' if File.exist?('local_ENV.rb')
 enable :sessions
 
+client = Mysql2::Client.new(:username => ENV['RDS_USERNAME'], :password => ENV['RDS_PASSWORD'], :host => ENV['RDS_HOSTNAME'], :port => ENV['RDS_PORT'], :database => ENV['RDS_DB_NAME'], :sock => '/tmp/mysql.sock')
+results = client.query("SELECT * FROM contacts")
+
+
+
 get '/' do
-  'Hello World!'
-  # erb :index, :layout => :layout 
+  resultSet = client.query('SELECT * FROM contacts')
+  contact_list = []
+  resultSet.each do |row|
+    contact_list << [[row['First_Name']], [row['Last_Name']], [row['Street']], [row['City']], [row['State']], [row['Zip']], [row['Phone_Number']]]
+  end
+
+  erb :index, :layout => :layout, locals: {contact_list: contact_list}
 end
 
 post '/add' do
+  first_name = params[:f_name]
+  last_name = params[:l_name]
+  street = params[:street]
+  city = params[:city]
+  state = params[:state]
+  zip = params[:zip]
+  phone_number = params[:p_num]
+  update = params[:contacts]
 
+  unless update == "no"
+    update = update.to_i + 1
+    client.query("UPDATE contacts SET First_Name = '#{first_name}', Last_Name = '#{last_name}', Street = '#{street}', City = '#{city}', State = '#{state}', Zip = '#{zip}', Phone_Number = '#{phone_number}' WHERE id = #{update}")
+  else
+    client.query("INSERT INTO contacts(First_Name, Last_Name, Street, City, State, Zip, Phone_Number)
+    VALUES('#{first_name}', '#{last_name}', '#{street}', '#{city}', '#{state}', '#{zip}', '#{phone_number}')")
+  end
+  redirect '/'
 end
---
+++ public/js/scripts.js
@@ -0,0 +1,29 @@
+// function updateOrAdd() {
+//     var update = document.getElementsByName("contacts").value;
+//     var first = document.getElementsByName("f_name");
+//     var last = document.getElementsByName("l_name");
+//     var street = document.getElementsByName("street");
+//     var city = document.getElementsByName("city");
+//     var state = document.getElementsByName("state");
+//     var zip = document.getElementsByName("zip");
+//     var phone = document.getElementsByName("p_num");
+//     var arr = [first, last, street, city, state, zip, phone];
+
+//     if (update == "no"){
+//         document.getElementById("mah_form").submit();
+//     }else{
+
+//     }
+// }
+
+// function fillFields() {
+//     var first = document.getElementsByName("f_name");
+//     var last = document.getElementsByName("l_name");
+//     var street = document.getElementsByName("street");
+//     var city = document.getElementsByName("city");
+//     var state = document.getElementsByName("state");
+//     var zip = document.getElementsByName("zip");
+//     var phone = document.getElementsByName("p_num");
+//     var arr = [first, last, street, city, state, zip, phone];
+
+// }
